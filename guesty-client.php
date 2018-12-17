cd429f26c5b7a7b5e66f346032e66856cc3155aa
Miklos Kaboldy <miklos.kaboldy@gmail.com>
2018-12-17 12:19:26 +0100

    POST and PUT methods fix, createguest and createreservation implemented

--- guesty-client.php
+++ guesty-client.php
@@ -34,18 +34,33 @@ class Guesty_Client {
     /**
      * Raw request, used internally
      * @param string $endpoint 
+     * @param string $method 
      * @param array $postfields 
+     * @return array $api_result
      */
-    private function request($endpoint, $postfields = []) {
+    private function request($endpoint, $method='GET', $postfields = []) {
 
         $ch = curl_init(); 
         curl_setopt($ch, CURLOPT_URL, $this->api_root . $endpoint);    
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // create request
+        if ('POST' == $method) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");  
+            curl_setopt($ch, CURLOPT_POST, true);                                                                   
+        }
+
+        // update request
+        if ('PUT' == $method) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  
+            // curl_setopt($ch, CURLOPT_PUT, true);                                                                   
+        }
 
         if (count($postfields) > 0) {
             $data_string = json_encode($postfields);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");  
-            curl_setopt($ch, CURLOPT_POST, true);                                                                   
+error_log($data_string);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
         }            
 
@@ -65,7 +80,7 @@ class Guesty_Client {
         }                                                                                                      
         if ( ($returnCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE )) !== 200) {
             curl_close($ch);
-            throw new Exception('HTTP return code ' . $returnCode . ' (' . $this->api_errors[$returnCode] . ') to ' . $this->api_root . $endpoint);
+            throw new Exception('HTTP return code ' . $returnCode . ' (' . $this->api_errors[$returnCode] . ') to ' . $this->api_root . $endpoint . ' ' .$result);
         }
         curl_close($ch);
         return json_decode($result, true);
@@ -102,6 +117,55 @@ class Guesty_Client {
             throw new Exception('ID , from and to must be specified to retrieve a calendar');
         }
         return $this->request('/listings/' . $id .'/calendar?from=' . $from . '&to=' . $to );
+    }
+
+    /**
+     * create a new guest object
+     * @param array $guest_data
+     * [
+     *  'firstName' => $firstName
+     *  'lastName' => $lastName
+     *  'email' => $email
+     * ]
+     * @return string $_id the ID of the user created
+     */
+    public function createguest($guest_data) {
+        try {
+            $new_guest = $this->request('/guests/','POST');
+            $updated_guest = $this->request('/guests/'. $new_guest['_id'],'PUT',$guest_data);
+            return $updated_guest;
+        } catch (Exception $e) {
+            $err = $e->getMessage();
+            throw new Exception('Failed to create new guest: ' . $err);
+        }
+    }
+    /**
+     * create a new reservation {"listingId": "59b928bb8e6bb31000219e58", "checkInDateLocalized": "2017-09-15", "checkOutDateLocalized": "2017-09-18", "status": "inquiry", "money":{"fareAccommodation": "500", "currency": "USD"}}
+     * @param string $listing_id
+     * @param string $guest_id
+     * @param string $checkin_date
+     * @param string $checkout_date
+     * @param string $guest_count
+     * @param string $total_goods
+     * @param string $total_sale
+     * @param string $already_paid
+     */
+    public function createreservation($listing_id, $guest_id, $checkin_date, $checkout_date, $guest_count, $total_goods, $total_sale, $already_paid) {
+        $reservation_data = array(
+            'listingId' => $listing_id,
+            'guestId' => $guest_id,
+            'checkInDateLocalized' => $checkin_date,
+            'checkOutDateLocalized' => $checkout_date,
+            'guestsCount' => $guest_count,
+            'status' => 'confirmed',
+            'money' => [
+                'fareAccommodation'=> $total_goods,
+                'guestTotalPrice' => $total_sale,
+                'alreadyPaid' => $already_paid,
+                'currency'=>'USD',
+            ]
+        );
+        return $this->request('/reservations/','POST',$reservation_data);
     }
 }
 
